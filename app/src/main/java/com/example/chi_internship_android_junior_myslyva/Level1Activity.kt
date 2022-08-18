06fd5810b248260033af9ac23a9717b72c9129a8
Yaroslava Myslyva <yaroslava.met@gmail.com>
2022-08-18 13:13:37 +0300

    Finished simplified version

--- app/src/main/java/com/example/chi_internship_android_junior_myslyva/Level1Activity.kt
+++ app/src/main/java/com/example/chi_internship_android_junior_myslyva/Level1Activity.kt
@@ -2,10 +2,18 @@ package com.example.chi_internship_android_junior_myslyva
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class Level1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_level1)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            findViewById<TextView>(R.id.textView).text =
+                findViewById<TextView>(R.id.textView).text.toString().toInt().inc().toString()
+
+        }
     }
 }
