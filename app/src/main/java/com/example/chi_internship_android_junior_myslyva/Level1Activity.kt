314c853e6d99097952017d6f436e82324599ab3c
Yaroslava Myslyva <yaroslava.met@gmail.com>
2022-08-18 12:05:30 +0300

    Added button and textview

---
+++ app/src/main/java/com/example/chi_internship_android_junior_myslyva/Level1Activity.kt
@@ -0,0 +1,11 @@
+package com.example.chi_internship_android_junior_myslyva
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Level1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_level1)
+    }
+}
