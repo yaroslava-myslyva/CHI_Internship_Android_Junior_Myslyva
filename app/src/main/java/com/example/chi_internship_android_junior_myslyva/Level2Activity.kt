a00c6bd2001e617b3b12e09105351a7d27e18651
Yaroslava Myslyva <yaroslava.met@gmail.com>
2022-08-20 22:18:45 +0300

    Appearance improvement

--- app/src/main/java/com/example/chi_internship_android_junior_myslyva/Level2Activity.kt
+++ app/src/main/java/com/example/chi_internship_android_junior_myslyva/Level2Activity.kt
@@ -2,16 +2,16 @@ package com.example.chi_internship_android_junior_myslyva
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TableLayout
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.chi_internship_android_junior_myslyva.databinding.ActivityLevel2Binding
 
 class Level2Activity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLevel2Binding
     val usersList: List<User> = mutableListOf(
-        User("Vasyl", 7),
+        User("Vasyl", 57),
         User("Kateryna", 12),
         User("Larysa", 69),
         User("Ivan", 18),
@@ -36,7 +36,9 @@ class Level2Activity : AppCompatActivity() {
             adapter = RVAdapter(usersList)
             layoutManager = LinearLayoutManager(this@Level2Activity)
             hasFixedSize()
-
+            addItemDecoration(
+                DividerItemDecoration(this@Level2Activity, RecyclerView.VERTICAL)
+            )
         }
 
     }
